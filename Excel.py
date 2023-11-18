8e1820f2a3bc963a1a9538154df73f822527c12b
Ilya <grinevich2018@mail.ru>
2023-11-18 13:27:59 +0600

    Fix some error

--- Excel.py
+++ Excel.py
@@ -89,11 +89,12 @@ def _save_excel_bd(DF: pandas.core.frame.DataFrame, DATABASE_NAME: str, GROUP: s
         :param GROUP: имя группы в формате "ПИН-221"
         :return: Сохраняет данные в эксель таблицу
     """
-    excel_header = ["Telegram ID", "Name", "GitHub"] + [f"ЛР{i}" for i in range(1, _kolvo_lab(DF=DF) + 1)] + [
-        "Points"] + ["" for _ in range(0, _kolvo_space(DF=DF))] + ["Подсчёт 1", "Подсчёт 2", "Подсчёт 3",
-                                                                   "Подсчёт 4", "Подсчёт 5", "Подсчёт 6",
-                                                                   "Подсчёт 7",
-                                                                   "Подсчёт 8"] + ["Статусы"]
+    excel_header = []
+    for col in range(0, len(list(DF.columns))):
+        if list(DF.columns)[col].split()[0] == "Unnamed:":
+            excel_header.append("")
+        else:
+            excel_header.append(DF.columns[col])
     try:
         _set_formula(DF=DF)
     except:
